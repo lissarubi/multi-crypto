b45eb2990693e778b24a84dbca2fff73812eb2be
Lissa Ferreira <lissadeveloper@gmail.com>
2020-04-24 20:03:28 -0400

    adding autokey and baconian

--- index.js
+++ index.js
@@ -9,39 +9,33 @@ baconian = require('./ciphers/baconian')
 exports.encryptBinary = (text) => {
     return binary.encryptBinary(text)
 }
-
 exports.decryptBinary = (binaryText) => {
     return binary.decryptBinary(binaryText)
 }
-
 exports.encryptMorse = (text) => {
     return morse.encryptMorse(text)
 }
-
 exports.decryptMorse = (morseText) => {
     return morse.decryptMorse(morseText)
 }
-
 exports.encryptCeaser = (text, shift) => {
     return ceaser.encryptCeaser(text, shift)
 }
-
+exports.bruteDecryptCeaser = (text) => {
+    return ceaser.bruteDecryptCeaser(text)
+}
 exports.brutedecryptCeaser = (text) => {
     return ceaser.bruteDecryptCeaser(text)
 }
-
 exports.encryptAtBash = (text) => {
     return atBash.encryptAtBash(text)
 }
-
 exports.decryptAtBash = (text) => {
     return atBash.decryptAtBash(text)
 }
-
 exports.encryptAffine = (text, a, b) => {
     return affine.encryptAffine(text, a, b)
 }
-
 exports.decryptAffine = (text, a, b) => {
     return affine.decryptAffine(text, a, b)
 }
